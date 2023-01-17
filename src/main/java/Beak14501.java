4877e7b39f2344056cb28054ce5db69df74c2fa9
hs-1991148-JungJaePark <104267255+LuizyHub@users.noreply.github.com>
2023-01-18 00:32:08 +0900

    14501 done

--- src/main/java/Beak14501.java
+++ src/main/java/Beak14501.java
@@ -4,6 +4,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Beak14501 {
+    public static int max(int a, int b) {
+        return (a >= b) ? a : b;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -15,13 +18,24 @@ public class Beak14501 {
             t[i] = Integer.parseInt(st.nextToken());
             p[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= Math.min(i,5); j++) {
-                max[i] = Math.max(max[i],max[i-j]);
+        for (int i = 0; i < Math.min(5,n); i++) {
+            for (int j = 1; j <= i; j++) {
+                if (t[i-j]>j)
+                    max[i] = max(max[i],max[i-j]);
             }
-            max[i+t[i]] = Math.max(max[i+t[i]], max[i] + p[i]);
+            max[i+t[i]] = max(max[i+t[i]], max[i] + p[i]);
         }
-        int maxPay = Math.max(max[n],Math.max(max[n-1],Math.max(max[n-2],Math.max(max[n-3],max[n-4]))));
-        System.out.println(maxPay);
+        for (int i = 5; i < n; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (t[i-j]>j)
+                    max[i] = max(max[i],max[i-j]);
+            }
+            max[i+t[i]] = max(max[i+t[i]], max[i] + p[i]);
+        }
+        for (int j = 1; j <= Math.min(5,n); j++) {
+            if (t[n-j]>j)
+                max[n] = max(max[n],max[n-j]);
+        }
+        System.out.println(max[n]);
     }
 }
